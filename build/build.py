075d80ee8359331ad33c58e8a01483813fc9cdba
Scott Graham <scottmg@chromium.org>
2018-05-23 21:42:02 -0700

    remove base64 and file_version_info

--- build/build.py
+++ build/build.py
@@ -612,7 +612,6 @@ def write_gn_ninja(path, root_gen_dir, options, windows_x64_toolchain):
         'base/debug/debugger_win.cc',
         'base/debug/profiler.cc',
         'base/debug/stack_trace_win.cc',
-        'base/file_version_info_win.cc',
         'base/files/file_enumerator_win.cc',
         'base/files/file_path_watcher_win.cc',
         'base/files/file_util_win.cc',
